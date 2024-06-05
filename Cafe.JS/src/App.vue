d9570c37eb89f168a651d411402bf0a98ef8e0ee
dayoung kim <canetaei@gmail.com>
2024-06-05 18:12:05 +0900

    0605

--- Cafe.JS/src/App.vue
+++ Cafe.JS/src/App.vue
@@ -1,85 +1,42 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import FinalPage from './components/FinalPage.vue'
+<script>
+import ReceiptBG from './views/ReceiptBG.vue';
+import OrderReceipt from './views/OrderReceipt.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ReceiptBG,OrderReceipt
+  },
+  data() {
+    return {
+      order: {
+        orderNumber: '253',
+        orderDate: '2024-06-05',
+        items: [
+          { id: 1, name: 'Product 1', quantity: 2, price: 10 },
+          { id: 2, name: 'Product 2', quantity: 1, price: 20 },
+          { id: 3, name: 'Product 3', quantity: 3, price: 15 }
+        ],
+        totalAmount: 85
+      }
+    };
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="App">
+    <OrderReceipt
+      :orderNumber="order.orderNumber"
+      :orderDate="order.orderDate"
+      :items="order.items"
+      :totalAmount="order.totalAmount"
+    />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+.container{color: linear-gradient (180deg, #5E7153 20%, #AF9544 50%, #D7A63C 75%, #FFB834 100%)};
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
+
---
+++ Cafe.JS/src/views/ReceiptBG.vue
@@ -0,0 +1,129 @@
+    <script setup>
+    console.log('cafe.js');
+    </script>
+
+    <script>
+    export default {
+    name: 'ReceiptBG',
+    props: {
+        container: Object,
+        logo: Object,
+        orderEnd: Object,
+        orderSecond: Object,
+        orderNumber: Object
+    }
+    }
+    </script>
+
+
+                    <template>
+
+                        <div class="container">
+            <div class="logo1">
+            <h1>cafe.js</h1>
+            </div>
+            <div class="logo2">
+            <h1>cafe.js</h1>
+            </div>
+
+            <div class="orderEnd">
+            <h2>주문이 완료 되었습니다!</h2>
+            </div>
+            <div class="orderSecond">
+            <h3>신용카드를 뽑은 후 <br> 출력된 영수증을 받아가세요!</h3>
+            </div>
+            <div class="orderNumber">
+            <h4>주문번호</h4>
+            </div>
+        </div>
+                    </template>
+
+                    <style scoped>
+
+            .container {
+            position: absolute;
+            width: 1080px;
+            height: 1920px;
+
+            background: linear-gradient(180deg, #5E7153 20%, #AF9544 50%, #D7A63C 75%, #FFB834 100%);}
+
+
+            .logo2 {
+            position: absolute;
+            width: 394px;
+            height: 146px;
+
+            font-family: 'DNF Bit Bit v2';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 100px;
+            line-height: 146px;
+            text-align: center;
+
+            color: #FFB834; }
+
+            .logo1 {
+            position: absolute;
+            width: 394px;
+            height: 146px;
+
+            font-family: 'DNF Bit Bit v2';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 100px;
+            line-height: 146px;
+            text-align: center;
+
+            color: #783E19; }
+
+            .orderEnd {
+            position: absolute;
+            width: 713px;
+            height: 82px;
+
+            font-family: 'BM JUA_TTF';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 80px;
+            line-height: 82px;
+            text-align: center;
+
+            color: #FFB834; }
+
+
+            .orderSecond {
+
+            position: absolute;
+            width: 530px;
+            height: 119px;
+
+            font-family: 'BM JUA_TTF';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 50px;
+            line-height: 52px;
+            text-align: center;
+
+            color: #FFB834; }
+
+
+            .orderNumber {
+                position: absolute;
+        width: 163px;
+        height: 52px;
+
+        font-family: 'BM JUA_TTF';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 50px;
+        line-height: 52px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+
+        color: #282808; }
+
+
+
+                    </style>
+
